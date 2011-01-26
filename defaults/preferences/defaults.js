72f81a1d093e1463ded3067f553dca2106dfd076
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2011-01-26 10:45:43 +0100

    Missing files. Oops.

---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,1 @@
+pref("kompose.path", "kompose");
---
+++ modules/misc.js
@@ -0,0 +1,40 @@
+var EXPORTED_SYMBOLS = ['wrapWithFormatting', 'parseToArrays'];
+
+const Ci = Components.interfaces;
+const Cc = Components.classes;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+const msgComposePrefs = Cc["@mozilla.org/preferences-service;1"]
+                          .getService(Ci.nsIPrefService)
+                          .getBranch("msgcompose.");
+const gHeaderParser = Cc["@mozilla.org/messenger/headerparser;1"]
+                      .getService(Ci.nsIMsgHeaderParser);
+
+/**
+ * Take some HTML and wrap it with the right body with the right style on it.
+ * The style is taken from the composition preferences.
+ * @param {String} aHtml The HTML to be enclosed in &lt;body&gt; tags.
+ * @return {String}
+ */
+function wrapWithFormatting (aHtml) {
+  let fgColor = msgComposePrefs.getCharPref("text_color");
+  let bgColor = msgComposePrefs.getCharPref("background_color");
+  let fontFace = msgComposePrefs.getCharPref("font_face");
+  let fontSize = msgComposePrefs.getCharPref("font_size");
+  let style =
+    "font-family: "+fontFace+"; " +
+    "font-size: "+fontSize+"; " +
+    "color: "+fgColor+"; " +
+    "background-color: "+bgColor+";"
+  ;
+  return ('<body style="'+style+'">'+aHtml+'</body>');
+}
+
+function parseToArrays(aMimeLine) {
+  let emails = {};
+  let fullNames = {};
+  let names = {};
+  let numAddresses = gHeaderParser.parseHeadersWithArray(aMimeLine, emails, names, fullNames);
+  return [names.value, emails.value];
+}
